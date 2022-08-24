5af4f0e9bbaa0dff37312dade03ae5bf2795f751
The-indigo <adepojuadeyemi11@gmail.com>
2022-08-23 20:11:26 -0400

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import Country from './components/Country';
 
 function App() {
+  const [country, setCountry] = useState([])
+  const [mode,setMode]=useState(false)
+  useEffect(() => {
+    const fetchData = async ()=>{
+      const response = await axios.get('https://restcountries.com/v3.1/all')
+      if (response.data) {
+        setCountry(response.data)
+      }
+      // console.log(response.data);
+    }
+    fetchData();
+  }, [])
+
+  const toggleMode = () => {
+    setMode(!mode)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className={`header ${mode ? "header-dark" : "header-light"}`}> 
+        <div className='header-container container'>
+                <h4>Where in the world?</h4>
+        <p onClick={toggleMode}>Dark Mode</p>
+        </div>
+      </div>
+      <div className='container'>
+
+        <div className='input-div'>
+          <input type='text' className='search-box' placeholder='Search for a country...' />
+          <div className='select-div'>
+          <select id="region" name="region" className='select-options'>
+  <option value="volv">Volvo</option>
+  <option value="saab">Saab</option>
+  <option value="fiat">Fiat</option>
+              <option value="audi">Audi</option>
+                <option value="volv">Volvo</option>
+  <option value="saab">Saab</option>
+  <option value="fiat">Fiat</option>
+  <option value="audi">Audi</option>
+</select>
+          </div>
+
+        </div>
+      
+        <main className='main'>
+            {country.map(
+        n => <Country key={n.name.official}  imageSource={n.flags.png}
+          countryName={n.name.common}
+          population={ n.population}
+          region={n.region }
+          capital={n.capital}
+        />
+      )}    
+</main>
+      </div>
     </div>
   );
 }
---
+++ src/components/Country.js
@@ -0,0 +1,36 @@
+import styled, { css } from "styled-components";
+import './Country.css';
+
+const StyledCountry = styled.div` 
+background:hsl(209, 23%, 22%);
+border-radius: 5px;
+overflow:hidden;
+color:hsl(0, 0%, 100%);
+width:90%;
+padding-bottom:1.5rem;
+
+margin-bottom:3rem;
+ @media (max-width:375.9px ) {
+    margin:0 auto;
+    margin-bottom:2rem
+  }
+  @media (min-width:376px ) {
+    width:22%;
+  }
+`
+
+const Country=({imageSource,countryName,population,region,capital}) => {
+    return (
+        <StyledCountry>
+            <img src={imageSource} />
+            <div className="details-div">
+   <h4>{countryName}</h4>
+            <p className="details">Population: <span className="details-1">{population}</span></p>
+            <p className="details">Region: <span className="details-1">{region}</span></p>
+            <p className="details">Capital: <span className="details-1">{capital}</span></p>
+            </div>
+
+        </StyledCountry>
+    )
+}
+export default Country;
